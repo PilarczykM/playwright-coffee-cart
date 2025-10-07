5886714d5117fc8b86e0c4ddbb57466faa6b5f8f
Marcin Pilarczyk <marcinpilarczyk@me.com>
2025-10-07 12:39:35 +0200

    Create homePage POM

---
+++ tests/poms/home-page.ts
@@ -0,0 +1,9 @@
+import type { Page } from "@playwright/test";
+import type { Navigation } from "../components/navigation";
+
+export class HomePage {
+  readonly navigation: Navigation;
+  constructor(readonly page: Page, navigation: Navigation) {
+    this.navigation = navigation;
+  }
+}
